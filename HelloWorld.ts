718b5bae5f4d5ac1a7843de226860281023f15cf
CSB Lab User Account <csb-lab@fresnostate.edu>
2019-08-29 17:07:58 -0700

    added print name function

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -11,8 +11,13 @@ class HelloWorld {
     sayHello(){
         console.log('Hello World!' + ' My name is ' + this.fullName + '.')
     }
+
+    printName() {
+        console.log('Name is: ' + this.fullName)
+    }
 } // end of class
 
 const myHelloInstance = new HelloWorld('Lucas Phan');
 
 myHelloInstance.sayHello();
+myHelloInstance.printName();
